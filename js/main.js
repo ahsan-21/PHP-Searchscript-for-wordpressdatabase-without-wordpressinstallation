873a32cee50cdee3cdd0a86ddcba72f49f73d08d
Ahsan Z <ehsanzia786@gmail.com>
2018-09-21 01:46:54 +0500

    auto search results on selecting any suggestions

--- js/main.js
+++ js/main.js
@@ -3,25 +3,31 @@
 */
 ;(function($) {
 
+	function sendAjaxRequest(searchValue) {
+		$(".loader").show();
+		$.ajax({
+			url: "ajax.php",
+			data: {
+				search: searchValue
+			},
+			success: function(result){
+				$(".loader").hide();
+				$("#suggestionListData").html(result);
+			},
+			error: function(error) {
+				console.log(error);
+			}
+		});
+	}
 	$(document).ready(function() {
-		console.log("Working");
 
 		$("#form1").submit(function(e) {
 			e.preventDefault();
-			$(".loader").show();
-			$.ajax({
-				url: "ajax.php",
-				data: {
-					search: $('#searchInput1').val()
-				},
-				success: function(result){
-					$(".loader").hide();
-					$("#suggestionListData").html(result);
-				},
-				error: function(error) {
-					console.log(error);
-				}
-			});
+			sendAjaxRequest($('#searchInput1').val());
+		});
+
+		document.getElementById('searchInput1').addEventListener("awesomplete-select", function(event) {
+			sendAjaxRequest(event.text.value);
 		});
 	});
 
